04a94b6f11bb334e6afff33f71c558f913db2922
Mahfujur Rahman Moon <37837514+Mahfujur51@users.noreply.github.com>
2022-04-12 01:07:08 +0600

    authenticate registern done 37

--- session/urls.py
+++ session/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 
-from .views import loginuser
+from .views import loginuser, logoutuser, registration
 
 app_name="session"
 urlpatterns = [
     path('login/',loginuser,name='loginuser'),
+    path('logout/',logoutuser,name='logoutuser'),
+    path('registration/',registration,name='registration'),
 ]
--- session/views.py
+++ session/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
+from .forms import SignupForm
+
 # Create your views here.
 
 
@@ -15,7 +17,7 @@ def loginuser(request):
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
-                login(request,user)
+                login(request, user)
                 return redirect('homeview')
             else:
                 messages.error(request, 'Invalid username or password')
@@ -24,3 +26,22 @@ def loginuser(request):
     else:
         form = AuthenticationForm()
     return render(request, 'session/login.html', {'form': form})
+
+
+def logoutuser(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully')
+    return redirect('homeview')
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for ' + username)
+            return redirect('tuition:loginuser')
+    else:
+        form = SignupForm()
+    return render(request, 'session/register.html', {'form': form})
